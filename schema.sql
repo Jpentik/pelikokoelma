a4751e897bd2a9b1239d21957ac22e52dc4eedcb
Jpentik <juha.ao.pentikainen@gmail.com>
2025-08-03 20:59:46 +0300

    Database tables for collections and games added

--- schema.sql
+++ schema.sql
@@ -4,3 +4,16 @@ CREATE TABLE users (
     password_hash TEXT
 );
 
+CREATE TABLE collections (
+    id INTEGER PRIMARY KEY,
+    title TEXT,
+    user_id INTEGER REFERENCES users
+);
+
+CREATE TABLE games (
+    id INTEGER PRIMARY KEY,
+    content TEXT,
+    sent_at TEXT,
+    user_id INTEGER REFERENCES users,
+    collection_id INTEGER REFERENCES collections
+);
